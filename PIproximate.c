6eb93ca6fcb2a09ad9a371b932f5900b9ccf7e5b
Soumyadip Mandal <mandal.soumyadip9714@gmail.com>
2019-01-16 20:26:03 +0530

    A simple code to approximate PI

---
+++ PIproximate.c
@@ -0,0 +1,28 @@
+// Here, using fraction 312689 / 99532
+// "The fraction 355/113 is incredibly close to pi, within a third of a millionth of the exact value." - http://davidbau.com
+// can also use 22/7
+
+//PIproximate - by unsuitable001
+// Who the heck am I? - read on github repo
+// https://github.com/unsuitable001/usnippet/blob/master/PIproximate.md
+#include<stdio.h>
+int main()
+{
+    FILE *out = fopen("PI", "a");
+    setvbuf(out, NULL, _IONBF, 0);
+    int div = 312689,r, rem;
+    r = div / 99532;
+    rem = div % 99532;
+    fprintf(out,"%d.",r);
+
+    while(rem != 0){
+        
+        div = rem * 10;
+        r = div / 99532;
+        rem = div % 99532;
+        fprintf(out,"%d",r);
+        flag++;
+    }
+    fclose(out);
+    return 0;
+}
