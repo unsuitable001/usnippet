759fc354352d35d8afb94790648d6d2db05c9200
Soumyadip Mandal <mandal.soumyadip9714@gmail.com>
2019-03-31 09:34:18 +0530

    Add files via upload

--- copyman.sh
+++ copyman.sh
@@ -2,10 +2,12 @@
 echo "Welcome to Copyman. A simple helper utility to copy files."
 echo "It can resume failed or terminated copies"
 echo "This uses cURL behind the hood"
+echo "DO NOT PUT PATH IN Quotation"
 echo "<Author: unsuitable001>"
 echo "File Location : "
 read Loc
 echo "File Destination : "
 read Dest
-cd $Dest
-curl -C -  -O "file://$Loc"
+if cd "$Dest";then
+	curl -C -  -O "file://$Loc"
+fi
