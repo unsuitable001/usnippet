84e0046571d9b1ccfc902ead418ee691a9d744d6
Soumyadip Mandal <mandal.soumyadip9714@gmail.com>
2019-04-02 09:39:00 +0530

    Adding Folder Support
    1. Adding folder support
    
    2.  Using `read -p` instead of `echo` for user input `prompt`

--- copyman.sh
+++ copyman.sh
@@ -4,10 +4,21 @@ echo "It can resume failed or terminated copies"
 echo "This uses cURL behind the hood"
 echo "DO NOT PUT PATH IN Quotation"
 echo "<Author: unsuitable001>"
-echo "File Location : "
-read Loc
-echo "File Destination : "
-read Dest
-if cd "$Dest";then
-	curl -C -  -O "file://$Loc"
+echo
+read -p "File Location : " Loc
+echo
+read -p "Directory? [Y/N] : " -n 1 -r isDir
+echo
+read -p "File Destination : " Dest
+echo
+if cd "$Dest"
+then
+	if [[ $isDir =~ ^[Yy]$ ]]
+	then
+		for f in $(ls -d -1 $Loc/*); do
+		curl -C -  -O "file://$f"
+		done
+	else
+		curl -C -  -O "file://$Loc"
+	fi
 fi
